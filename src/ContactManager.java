db0cf8fef74fcc10bc8876afc7d6717dd9747393
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-05-11 15:15:09 -0500

    feat: add search functionality

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -3,13 +3,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ContactManager {
     public static Scanner sc = new Scanner(System.in);
+    private String name;
+    private String number;
+
+    public ContactManager(String name, String number) {
+        setName(name);
+        setPhoneNumber(number);
+    }
+
+    // getters
+    public static String getName(ContactManager ele) {
+        return ele.name;
+    }
+    public static String getCategory(ContactManager ele) {
+        return ele.number;
+    }
+
+    //changes movie info, setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhoneNumber(String number) {
+        this.number = number;
+    }
+
     public static void main(String[] args) throws IOException {
         String directory = "data";
         String filename = "contacts.txt";
@@ -42,44 +63,60 @@ public class ContactManager {
             int userInput = sc.nextInt();
 
             if (userInput < 1 || userInput > 5) {
+
                 System.out.println("Invalid input, please try again: ");
+
             } else if (userInput == 1) {
+
                 System.out.println("Name | Phone Number |");
                 System.out.println("-----------------------");
                 printContacts(dataFile); //calling the method below
                 wrongInput = false;
 
             } else if (userInput == 2) {
-                System.out.println("Please enter a new contact's name:");
-                String contactName = sc.next();
-                System.out.println("Please enter contact's phone number:");
-                String contactNumber = sc.next();
 
-                Files.write(
-                        Paths.get("data", "contacts.txt"),
-                        Arrays.asList(contactName + " | " + contactNumber + " | "),
-                        StandardOpenOption.APPEND
-                );
+                boolean answer = true;
+                while (answer) {
+
+                    System.out.println("Please enter a new contact's first and last name:");
+                    String firstName = sc.next(); //this catches the first input
+                    String lastName = sc.next(); // catches the second input
+                    System.out.println("Please enter contact's phone number:");
+                    String contactNumber = sc.next();
+
+                    String contactName = firstName + " " + lastName;
+
+                    Files.write(
+                            Paths.get("data", "contacts.txt"),
+                            Arrays.asList(contactName + " | " + contactNumber + " | "),
+                            StandardOpenOption.APPEND
+                    );
+
+                    System.out.println("Would you like to enter another contact? [y/N]");
+                    String response = sc.next();
+
+                    answer = response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yes");
+                }
 
                 wrongInput = false;
+
             } else if (userInput == 3) {
-                System.out.println("Search contact");
+
+                searchContacts(dataFile);
                 wrongInput = false;
+
             } else if (userInput == 4) {
+
                 System.out.println("delete contacts");
                 wrongInput = false;
+
             } else if (userInput == 5) {
+
                 System.out.println("exit program");
                 wrongInput = false;
+
             }
         }
-
-
-//printing the contact list:
-
-
-
-
     }
 
     //using IOException instead try/catch method:
@@ -94,4 +131,16 @@ public class ContactManager {
 
     }
 
+    public static void searchContacts(Path filePath) throws IOException {
+        List<String> fileContents = Files.readAllLines(filePath);
+
+        System.out.println("Please enter name you would like to search:");
+        String contactName = sc.next();
+
+        for (int i = 0; i < fileContents.size(); i++) {
+            if (fileContents.get(i).contains(contactName)) {
+                System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
+            }
+        }
+    }
 }
