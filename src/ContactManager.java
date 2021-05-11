33a107525940b29f9e963985cd8f7d3319fc2e1b
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-05-11 10:02:19 -0500

    created contacts.txt

---
+++ src/ContactManager.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContactManager {
+    public static void main(String[] args) throws IOException {
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (! Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+    }
+}
