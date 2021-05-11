5ef30ebd419504adae336d8ec49221905209f40b
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-05-11 11:55:24 -0500

    feat: added display contacts/user interaction

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -6,8 +6,10 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class ContactManager {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
         String directory = "data";
         String filename = "contacts.txt";
@@ -22,5 +24,65 @@ public class ContactManager {
         if (! Files.exists(dataFile)) {
             Files.createFile(dataFile);
         }
+
+
+        boolean wrongInput = true;
+
+        System.out.print("""
+                1. View contacts.
+                2. Add a new contact.
+                3. Search a contact by name.
+                4. Delete an existing contact.
+                5. Exit.
+                Enter an option (1, 2, 3, 4 or 5):
+                 """);
+
+        while (wrongInput) {
+
+            int userInput = sc.nextInt();
+
+            if (userInput < 1 || userInput > 5) {
+                System.out.println("Invalid input, please try again: ");
+            } else if (userInput == 1) {
+                System.out.println("view contacts");
+                wrongInput = false;
+            } else if (userInput == 2) {
+                System.out.println("Add contact");
+                wrongInput = false;
+            } else if (userInput == 3) {
+                System.out.println("Search contact");
+                wrongInput = false;
+            } else if (userInput == 4) {
+                System.out.println("delete contacts");
+                wrongInput = false;
+            } else if (userInput == 5) {
+                System.out.println("exit program");
+                wrongInput = false;
+            }
+        }
+
+
+//printing the contact list:
+//        Files.write(
+//                Paths.get("data", "contacts.txt"),
+//                Arrays.asList("Nicholas (210) 876-0987"), // list with one item
+//                StandardOpenOption.APPEND
+//        );
+
+        printContacts(dataFile); //calling the psv below
+
     }
+
+    //using IOException instead try/catch method:
+    public static void printContacts(Path filePath) throws IOException {
+
+        System.out.println();
+        List<String> fileContents = Files.readAllLines(filePath);
+        for (int i = 0; i < fileContents.size(); i++) {
+            System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
+        }
+
+
+    }
+
 }
